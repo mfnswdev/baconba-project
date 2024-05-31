bfe09ec73e5a10dd68a0f136ba47f9ee8f62ba0f
GiuseppeMota <giuseppemota@gmail.com>
2024-05-31 16:38:47 -0300

    fix sln

---
+++ tests/BaconBA.API.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BaconBA.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/BaconBA.Data.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BaconBA.Data.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
